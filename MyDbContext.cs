6775386428628478a7146823af937625521a83db
Trần Quang Đạt <dat66mc@gmail.com>
2024-10-03 19:55:04 +0700

    Dataservice

---
+++ MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using QLBH;
+using System.Data.Entity;
+
+namespace QLBH.DataService
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("name = constr")
+        {
+
+        }
+        public virtual DbSet<Models.Category> Categories { get; set; }
+        public virtual DbSet<Models.Employee> Employees { get; set; }
+        public virtual DbSet<Models.Product> Products { get; set; }
+        public virtual DbSet<Models.Mobile> Mobiles { get; set; }
+        public virtual DbSet<Models.Cat_Mobile> Cats { get; set; }
+        public virtual DbSet<Models.User> Users { get; set; }
+
+    }
+}
